18833746468101defcf8ff7bc336fb240a1644d4
RayneAS <raynealvesantos@gmail.com>
2024-07-19 00:37:03 -0300

    Update 05_descritivas_PNS.R
    update para ver descritivas

--- code/05_descritivas_PNS.R
+++ code/05_descritivas_PNS.R
@@ -49,3 +49,66 @@ names(PNS_2019) <- c("num_pes_dom", "cond_dom", "sexo","idade", "cor", "conj_mor
                      "afast_1ano_2ano", "afast_acima_2ano", "sevidor_pub", "carteira_assin",
                      "ativ_empresa", "rendimento_trab", "horas_trab", "internado", 
                      "qtas_internado" , "tempo_internado_meses", "tempo_internado_dias")
+
+#Mudar para numeric
+PNS_2019 <- PNS_2019 %>% 
+  mutate_at(c('cond_dom', 'sexo','cor', 'afast','qtas_internado',
+              'tempo_internado_meses', 'tempo_internado_dias), as.numeric)
+
+
+##Filtros para analise especifica ----------------------------------------------
+#Mantem as pessoas afastadas do trabalho 
+PNS_sample <- PNS_2019 %>% 
+  filter(afast == 1)
+
+#Mantem apenas o chefe do domicilio e homem 
+PNS_sample <- PNS_sample  %>% 
+  filter(cond_dom == 1)
+
+#Mantem apenas homem 
+PNS_sample <- PNS_sample %>% 
+  filter(sexo == 1)
+
+#mantem apenas motivos de afastamento especificos 
+PNS_sample <- PNS_sample  %>% 
+  filter(motivo_afast == 3 | motivo_afast == 5)
+
+freq <- table(PNS_sample$motivo_afast)
+freq
+
+#Mantem apenas idade entre 15 e 65 anos 
+PNS_sample <- PNS_sample %>% 
+  filter(idade >= 15 & idade <= 65)
+
+freq <- table(addNA(PNS_sample$doenca_trab))
+freq
+
+freq <- table(addNA(PNS_sample$carteira_assin))
+freq
+
+freq <- table(addNA(PNS_sample$conj_mora))
+freq
+
+freq <- table(addNA(PNS_sample$cor))
+freq
+
+freq <- table(addNA(PNS_sample$tempo_afast))
+freq
+
+avg <- mean(PNS_sample$tempo_afast, na.rm = TRUE)
+avg
+
+
+
+freq <- table(addNA(PNS_sample$internado))
+freq
+
+freq <- table(addNA(PNS_sample$tempo_internado_dias))
+freq
+
+avg <- mean(PNS_sample$tempo_internado_dias, na.rm = TRUE)
+avg
+
+avg <- mean(PNS_sample$tempo_internado_meses, na.rm = TRUE)
+avg
+
