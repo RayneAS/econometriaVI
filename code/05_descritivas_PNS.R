d729ae4fef6791e375336c372c93095688d06337
RayneAS <raynealvesantos@gmail.com>
2024-07-24 23:19:55 -0300

    Update 05_descritivas_PNS.R
    Arruma as estatisticas descritivas para colocar no projeto

--- code/05_descritivas_PNS.R
+++ code/05_descritivas_PNS.R
@@ -274,7 +274,8 @@ PNS_2019$idind <- paste(PNS_2019$iddom, PNS_2019$sexo, PNS_2019$dia_nas,
 PNS_2019 <- PNS_2019 %>% 
   mutate_at(c('cond_dom', 'sexo','cor', 'afast','qtas_internado',
               'tempo_internado_meses', 'tempo_internado_dias', 
-              'dias_deixou_trab_saude','dias_acamado'), as.numeric)
+              'dias_deixou_trab_saude','dias_acamado', 
+              'motivo_deixou_trab_saude'), as.numeric)
 
 
 # write.csv(PNS_2019, file = "D:/rayne/Documents/dados_econometria_VI/PNS_2019.csv",
@@ -285,29 +286,158 @@ PNS_2019 <- PNS_2019 %>%
 PNS_2019 <- PNS_2019 %>% 
   select(iddom,idind, everything())
 
+### PRODUZ TABELA COM MOTIVOS DE DEIXAR O TRABALHO POR DOENÇA--------------
 
-#Mantem as pessoas que deixaram de trabalhar por doença 
-PNS_sample <- PNS_2019 %>% 
-  filter(deixou_trab_saude == 'sim')
+##TABELA 1 -----------------------------------------------------------------
 
-freq <- table(addNA(PNS_sample$motivo_deixou_trab_saude))
+# Mantém as pessoas que deixaram de trabalhar por doença, são chefes e homens
+PNS_sample <- PNS_2019 %>%
+  filter(cond_dom == 1 & sexo == 1 & deixou_trab_saude == "sim")
+
+freq <- table((PNS_sample$motivo_deixou_trab_saude))
 freq
+
+
+# Contar o número total de chefes do sexo masculino
+total_chefes <- PNS_2019 %>%
+  filter(cond_dom == 1 & sexo == 1) %>%
+  nrow()
+
+# Contar o número de chefes do sexo masculino que deixaram de trabalhar por problemas de saúde
+total_chefes_doentes <- PNS_sample %>%
+  nrow()
+
+# Calcular a porcentagem de chefes que deixaram de trabalhar por problemas de saúde
+porcentagem_chefes_doentes <- (total_chefes_doentes / total_chefes) * 100
+
+# Contar o número de chefes do sexo masculino por tipo de motivo
+motivos <- PNS_sample %>%
+  count(motivo_deixou_trab_saude) %>%
+  mutate(porcentagem = (n / total_chefes_doentes) * 100) %>%
+  filter(motivo_deixou_trab_saude != 3) # Remove categoria 3
+
+# Renomear os valores para rótulos mais amigáveis
+motivos$motivo_deixou_trab_saude <- recode(motivos$motivo_deixou_trab_saude,
+                                           `1` = "Problemas nos ossos e articulações",
+                                           `2` = "Dor de cabeça ou enxaqueca",
+                                           `4` = "Problema odontológico",
+                                           `5` = "Problemas respiratórios",
+                                           `6` = "Problemas gastrointestinais",
+                                           `7` = "Dengue, Chikungunya, Zika ou Febre amarela",
+                                           `8` = "Problemas cardiovasculares",
+                                           `9` = "Diabetes",
+                                           `10` = "Câncer",
+                                           `11` = "Problemas neurológicos",
+                                           `12` = "Saúde mental",
+                                           `13` = "Lesões ou fraturas por acidentes ou violência",
+                                           `14` = "Outro problema de saúde"
+)
+
+# Criar o data frame com as estatísticas dos chefes doentes
+estatisticas <- data.frame(
+  Estatística = c(
+    "Total de Chefes (Sexo Masculino)", 
+    "Total de Chefes (Sexo Masculino) que Deixaram de Trabalhar por Saúde", 
+    "Porcentagem de Chefes (Sexo Masculino) que Deixaram de Trabalhar por Saúde (%)",
+    "Problemas nos ossos e articulações (%)",
+    "Dor de cabeça ou enxaqueca (%)",
+    "Problema odontológico (%)",
+    "Problemas respiratórios (%)",
+    "Problemas gastrointestinais (%)",
+    "Dengue, Chikungunya, Zika ou Febre amarela (%)",
+    "Problemas cardiovasculares (%)",
+    "Diabetes (%)",
+    "Câncer (%)",
+    "Problemas neurológicos (%)",
+    "Saúde mental (%)",
+    "Lesões ou fraturas por acidentes ou violência (%)",
+    "Outro problema de saúde (%)"
+  ),
+  Valor = c(
+    total_chefes,
+    total_chefes_doentes,
+    porcentagem_chefes_doentes,
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Problemas nos ossos e articulações"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Dor de cabeça ou enxaqueca"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Problema odontológico"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Problemas respiratórios"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Problemas gastrointestinais"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Dengue, Chikungunya, Zika ou Febre amarela"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Problemas cardiovasculares"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Diabetes"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Câncer"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Problemas neurológicos"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Saúde mental"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Lesões ou fraturas por acidentes ou violência"],
+    motivos$porcentagem[motivos$motivo_deixou_trab_saude == "Outro problema de saúde"]
+  )
+)
+
+# Usar o stargazer para criar uma tabela
+stargazer(estatisticas, type = "text", summary = FALSE, rownames = FALSE, 
+          title = "Principais Motivos de Saúde para Ausência em Atividades Diárias")
+
+stargazer(estatisticas, type = "latex", summary = FALSE, rownames = FALSE, 
+          title = "Principais Motivos de Saúde para Ausência em Atividades Diárias")
+
+##TABELA 2 -----------------------------------------------------------------
 
 freq <- table(addNA(PNS_sample$rel_trab_deixou_trab_saude))
 freq
 
-freq <- table(addNA(PNS_sample$dias_deixou_trab_saude))
-freq
+relacio_trabalho <- PNS_sample %>%
+  count(rel_trab_deixou_trab_saude) %>%
+  mutate(porcentagem = (n / total_chefes_doentes) * 100)
+
+
+relacio_trabalho$rel_trab_deixou_trab_saude <- recode(relacio_trabalho$rel_trab_deixou_trab_saude,
+                                           `1` = "Sim",
+                                           `2` = "Não",
+                                           `3` = "Não trabalhava/não trabalhou")
+
+
+print(relacio_trabalho)
+
+# Filtrar os dados para incluir apenas os casos onde 
+#"rel_trab_deixou_trab_saude" e "acamado" são "Sim"
+rel_trab_sim <- PNS_sample %>%
+  filter(rel_trab_deixou_trab_saude == 1) %>%
+  summarise(porcentagem = (n() / total_chefes_doentes) * 100) %>%
+  mutate(Categoria = "O motivo de saúde estava relacionado ao trabalho (%)")
+
+acamado_sim <- PNS_sample %>%
+  filter(acamado == "sim") %>%
+  summarise(porcentagem = (n() / total_chefes_doentes) * 100) %>%
+  mutate(Categoria = "Esteve acamado recentemente")
+
+
+doença_sim <- PNS_sample %>%
+  filter(tem_doenca== "sim") %>%
+  summarise(porcentagem = (n() / total_chefes_doentes) * 100) %>%
+  mutate(Categoria = "Recebeu diagnóstico de alguma doença crônica, física ou mental, ou longa duração")
+
+# Combine as tabelas em uma única tabela
+tabela_final <- bind_rows(rel_trab_sim, acamado_sim, doença_sim) %>%
+  select(Categoria, porcentagem)
+
+# Exibir a tabela final
+print(tabela_final)
+
+stargazer(tabela_final, type = "latex", summary = FALSE, rownames = FALSE, 
+          title = "Estatisticas da amostra ")
+
 
 freq <- table(addNA(PNS_sample$acamado))
-freq
-
-freq <- table(addNA(PNS_sample$dias_acamado))
 freq
 
 freq <- table(addNA(PNS_sample$tem_doenca))
 freq
 
+freq <- table(addNA(PNS_sample$dias_deixou_trab_saude))
+freq
+
+freq <- table(addNA(PNS_sample$dias_acamado))
+freq
 
 #Mantem as pessoas que deixaram de trabalhar por motivo relacionado ao trabalho 
 PNS_sample_2 <- PNS_sample %>% 
@@ -337,7 +467,7 @@ freq
 freq <- table(addNA(PNS_sample_2$sit_saude_2))
 freq
 
-#Analise por domicilio -----------------------------------------------------
+#Analise por domicilio usando a var deixou de trabalhar por doença -----------------------------------------------------
 
 library(dplyr)
 library(stargazer)
@@ -396,6 +526,85 @@ estatisticas <- data.frame(
   Estatística = c("Total de Chefes (Sexo Masculino)", "Total de Chefes (Sexo Masculino) que Deixaram de Trabalhar por Saúde", 
                   "Porcentagem de Chefes (Sexo Masculino) que Deixaram de Trabalhar por Saúde",
                   "Total de Cônjuges na Base", "Total de Cônjuges na Amostra dos Chefes doentes", 
+                  "Total de Cônjuges que Auxiliaram", 
+                  "Porcentagem de Cônjuges que Auxiliaram", 
+                  "Total de Cônjuges que Auxiliaram e Trabalham", 
+                  "Porcentagem de Cônjuges que Auxiliaram e Trabalham"),
+  Valor = c(total_chefes, total_chefes_doentes, 
+            porcentagem_chefes_doentes, total_conjuges_base, 
+            total_conjuges_amostra, total_conjuges_auxiliaram, 
+            porcentagem_auxiliaram, total_auxiliaram_trabalhavam, 
+            porcentagem_auxiliaram_trabalhavam)
+)
+
+
+# Usar o stargazer para criar uma tabela
+stargazer(estatisticas, type = "text", summary = FALSE, rownames = FALSE, title = "Estatísticas Descritivas")
+
+# Usar o stargazer para criar uma tabela em LaTeX
+stargazer(estatisticas, type = "latex", summary = FALSE, rownames = FALSE, title = "Estatísticas Descritivas")
+
+
+
+#Analise por domicilio usando a var AFASTAMENTO -----------------------------------------------------
+
+library(dplyr)
+library(stargazer)
+
+# Filtrar para domicílios onde o chefe (sexo masculino) deixou de trabalhar por problemas de saúde
+chefe_doente <- PNS_2019 %>%
+  filter(cond_dom == 1 & sexo == 1 & afast == 1 & motivo_afast ==3 | motivo_afast == 5 ) %>%
+  select(iddom)
+
+# Contar o número total de chefes do sexo masculino
+total_chefes <- PNS_2019 %>%
+  filter(cond_dom == 1 & sexo == 1) %>%
+  nrow()
+
+# Contar o número de chefes do sexo masculino que deixaram de trabalhar por problemas de saúde
+total_chefes_doentes <- chefe_doente %>%
+  nrow()
+
+# Calcular a porcentagem de chefes que deixaram de trabalhar por problemas de saúde
+porcentagem_chefes_doentes <- (total_chefes_doentes / total_chefes) * 100
+
+# Contar o número total de cônjuges do sexo feminino na base
+total_conjuges_base <- PNS_2019 %>%
+  filter(cond_dom == 2 & sexo == 2) %>%
+  nrow()
+
+# Filtrar para cônjuges do sexo feminino que residem nos domicílios dos chefes doentes
+conjuges <- PNS_2019 %>%
+  filter(iddom %in% chefe_doente$iddom & cond_dom == 2 & sexo == 2)
+
+# Contar o número de cônjuges na amostra dos chefes doentes
+total_conjuges_amostra <- conjuges %>% nrow()
+
+# Filtrar para cônjuges do sexo feminino que auxiliaram em algo
+conjuges_auxiliaram <- conjuges %>%
+  filter(aux_cuidado_pess == "sim" | aux_companhia == "sim" | aux_outros == "sim")
+
+# Contar o número de cônjuges que auxiliaram em algo
+total_conjuges_auxiliaram <- conjuges_auxiliaram %>% nrow()
+
+# Calcular a porcentagem de cônjuges que auxiliaram em algo
+porcentagem_auxiliaram <- (total_conjuges_auxiliaram / total_conjuges_amostra) * 100
+
+# Filtrar para cônjuges que trabalhavam e auxiliaram em algo
+conjuges_auxiliaram_trabalhavam <- conjuges_auxiliaram %>%
+  filter(trabalhou == "sim")
+
+# Contar o número de cônjuges que auxiliaram e trabalhavam
+total_auxiliaram_trabalhavam <- conjuges_auxiliaram_trabalhavam %>% nrow()
+
+# Calcular a porcentagem de cônjuges que auxiliaram e trabalhavam
+porcentagem_auxiliaram_trabalhavam <- (total_auxiliaram_trabalhavam / total_conjuges_auxiliaram) * 100
+
+# Criar um data frame com as estatísticas
+estatisticas <- data.frame(
+  Estatística = c("Total de Chefes (Sexo Masculino)", "Total de Chefes (Sexo Masculino) que Deixaram de Trabalhar por Saúde", 
+                  "Porcentagem de Chefes (Sexo Masculino) Afastados por choque de saúde",
+                  "Total de Cônjuges na Base", "Total de Cônjuges na Amostra dos Chefes Afastados", 
                   "Total de Cônjuges que Auxiliaram", 
                   "Porcentagem de Cônjuges que Auxiliaram", 
                   "Total de Cônjuges que Auxiliaram e Trabalham", 
